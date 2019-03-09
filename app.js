653492bf3dc3f8ecff897bfec3f1863010a89b3e
Nandanhn <hnnandan15@gmail.com>
2019-03-10 02:08:49 +0530

    usage of chalk module

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const validator=require('validator')
-const getNotes=require("./notes")
-var msg=getNotes()
-console.log(msg)
+const chalk=require('chalk')
 
-// const add=require("./utils")
-// var sum=add(-4,-2)
-// console.log(sum)
+const log=console.log;
 
-console.log(validator.isEmail("nandans"))
-console.log(validator.isEmail("hnnandan15@gmail.com"))
 
-console.log(validator.isURL("https://nandan.com"))
-console.log(validator.isURL("https/nandan.com"))
+log(chalk.green("Success!"))
+log(chalk.bold.green("Success!"))
+log(chalk.inverse.bold.green("Success!"))
+
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+ 
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+ 
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+ 
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+ 
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'
+));
+ 
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+ 
+
+
